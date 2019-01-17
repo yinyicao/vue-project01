aed9a69cb1659fef1c5fc04f41dbf788a4be0415
yyc007 <34782655@qq.com>
2019-01-17 17:26:58 +0800

    使用ElementUI的tabs标签设置路由

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
   <div id="app">
     <!--<img src="./assets/logo.png">-->
-    <router-link to="/addRowComponent">Go to AddRowComponent</router-link>
-    <router-link to="/tableComponent">Go to TableComponent</router-link>
-    <router-view/>
+    <el-tabs v-model="activeName2" type="border-card" @tab-click="handleClick">
+      <el-tab-pane label="双向绑定" name="first"><router-view/></el-tab-pane>
+      <el-tab-pane label="动态加行" name="second"><router-view/></el-tab-pane>
+    </el-tabs>
+    <!--<router-link to="/addRowComponent" tag="button">Go to AddRowComponent</router-link>-->
+    <!--<router-link to="/tableComponent" tag="button">Go to TableComponent</router-link>-->
+    <!--<router-view/>-->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      activeName2: 'first'
+    }
+  },
+  methods: {
+    handleClick (tab, event) {
+      if (tab.$el.id === 'pane-first') {
+        this.$router.push({ path: '/tableComponent' })
+      } else {
+        this.$router.push({ path: '/addRowComponent' })
+      }
+      // console.log(tab.$el, event)
+    }
+  }
 }
 </script>
 
