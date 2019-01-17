6c0a3e7fa6b29987db5e458c88c839abc82e2bbb
yyc007 <34782655@qq.com>
2019-01-17 16:12:31 +0800

    删除自动生成的helloworld.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
 import AddRowComponent from '@/components/AddRowComponent'
 import TableComponent from '@/components/TableComponent'
 
